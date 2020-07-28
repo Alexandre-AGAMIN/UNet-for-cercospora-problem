634f089645d4f6156f063cf3dd5be7f82a36d365
Alexandre-AGAMIN <alexandrepm2810@gmail.com>
2020-07-28 01:50:01 -0300

    Create train of model

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from keras_segmentation.models.unet import unet
 
-model = unet()
+model = unet(n_classes=2, input_height=512, input_width=512)
+
+model.train(
+    train_images="RecorteCercospora/Imagem/",
+    train_anotations="RecorteCercospora/Mascara/",
+    checkpoints_pth="Checkpoints",
+    epochs=100
+)
+
+
