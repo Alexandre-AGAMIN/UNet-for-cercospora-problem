733a9b370aee916c62be7a46144d20ae9bdb076c
Alexandre-AGAMIN <alexandrepm2810@gmail.com>
2020-07-29 00:52:21 -0300

    Arrumando

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from keras_segmentation.models.unet import unet
-import matplotlib as plt
+import matplotlib.pylab  as plt
+import matplotlib.image  as pltimg
 import cv2 as cv
 import numpy as np
+from keras.callbacks import ModelCheckpoint as check
 
 imagesForTrain = \
     "C:\\Users\\alexa\\OneDrive - ufu.br\\ProjetoWesley\\UNet-for-cercospora-problem\\RecorteCercospora\\Imagem"
@@ -23,30 +25,34 @@ model.train(
     verify_dataset=False,
     input_height=512, input_width=512,
     val_annotations=maskForValidation,
-    epochs=2
+    epochs=5
 )
 
-imageInputFroPrediction = \
-    "C:\\Users\\alexa\\OneDrive - ufu.br\\ProjetoWesley\\UNet-for-cercospora-problem\\predictResults\\sample_0801.png"
+imageInputFroPrediction = "C:\\Users\\alexa\\OneDrive - ufu.br\\ProjetoWesley\\UNet-for-cercospora-problem\\predictResults\\ImagePredict\\sample_0961.png"
+fileTosaveOutput = "C:\\Users\\alexa\\OneDrive - ufu.br\\ProjetoWesley\\UNet-for-cercospora-problem\\predictResults\\predictResults"
+print(model.evaluate_segmentation( inp_images_dir=imagesForValidation  , annotations_dir=maskForValidation ))
 
 
-saida = model = model.predict_segmentation(
+saida =  model.predict_segmentation(
     inp=imageInputFroPrediction,
     out_fname=
     "C:\\Users\\alexa\\OneDrive - ufu.br\\ProjetoWesley\\UNet-for-cercospora-problem\\predictResults\\predictResults\\out.png"
 )
 
-plt.imshow(saida)
+
+print(type(saida))
+
+cv.imshow("Bora ver",saida)
 
 
 
 ###Como abrir imagem com o OPENCV############
 
-# img = cv.imread(maskForTrain+"\\sample_0001.png")
-#
-#
-# print(np.shape(img))
-#cv.imshow("mask", img)
+## img = cv.imread(maskForTrain+"\\sample_0001.png")
+##img = cv.imread(imageInputFroPrediction)
+##
+##print(type(img))
+##cv.imshow("mask", img)
 #cv.imshow("mask2", imgt)
 #print(type(img))
 
